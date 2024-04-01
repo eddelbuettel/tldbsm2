02b8358f5d042c920182ebeacded680fa39ab8bf
Dirk Eddelbuettel <edd@debian.org>
2024-04-01 15:41:35 -0500

    Also delete on sync plus two conditioners

--- apis/r/man/roxygen/templates/param-repr-read.R
+++
@@ -1,5 +0,0 @@
-#' @param repr Representation of the sparse matrix to return; choose from:
-#' \itemize{
-#'  \item \dQuote{\code{T}}: returns a
-#'   \code{\link[Matrix:TsparseMatrix-class]{TsparseMatrix}}
-#' }
